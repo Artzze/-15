2e037fa8e92b52bb4e696bfeb17a783507993a3f
Artzze <Artzze@FOXPC>
2024-06-26 10:27:37 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class bibl
+    {
+        public static void Sum(int chislo, int B)
+        {
+            int ProverkaKratnosti = 1, Proverka4 = 0, ProverkaB = 0;
+            int[] MasChisla = new int[4];
+            for (int i = 0; i < MasChisla.Length; i++)
+            {
+                MasChisla[i] = chislo % 10;
+                chislo /= 10;
+                ProverkaKratnosti *= MasChisla[i]; 
+            }
+            for (int i = 0; i < MasChisla.Length; i++)
+            {
+                if (MasChisla[i] == 4 && Proverka4 == 0)
+                {
+                    Console.WriteLine("Число имеет цифру 4");
+                    Proverka4++;
+                }
+                if (MasChisla[i] == B && ProverkaB == 0)
+                {
+                    Console.WriteLine("Число имеет значение B");
+                    ProverkaB++;
+                }
+            }
+            if (ProverkaKratnosti % 4 == 0)
+                Console.WriteLine("Число кратно 4");
+            else
+                Console.WriteLine("Число не кратно 4");
+        }
+    }
+}
---
+++ практическая 15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace практическая_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите четырехзначное число");
+            bool f = int.TryParse(Console.ReadLine(), out int chislo);
+                if (chislo >= 1000 && chislo <= 9999 && f)
+                {
+                    Console.WriteLine("Введите B");
+                    int B = Convert.ToInt32(Console.ReadLine());
+                    bibl.Sum(chislo, B);
+                    Console.ReadKey();
+                }
+            else
+              Console.WriteLine("Необходимо ввести четырехзначное число");
+            Console.ReadKey();
+        }
+    }
+}
